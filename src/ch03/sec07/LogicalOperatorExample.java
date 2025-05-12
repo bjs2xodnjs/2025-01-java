6403dda0951d18e54ae2ffa72302afbaac6cec4f
taewon <taewon1918@gmail.com>
2025-05-12 18:06:58 +0900

    for 문 조건문, 랜덤값 만들기 수정, for문에 대해서 공부

---
+++ src/ch03/sec07/LogicalOperatorExample.java
@@ -0,0 +1,10 @@
+package ch03.sec07;
+
+public class LogicalOperatorExample {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 0;
+
+        System.out.printf("%d > %d : %b\n");
+    }
+}
