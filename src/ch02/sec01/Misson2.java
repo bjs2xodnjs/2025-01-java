12b215dda116347b4f3023fa918a991c9f8384ba
taewon <taewon1918@gmail.com>
2025-05-12 09:24:11 +0900

    for 문 조건문, 랜덤값 만들기 수정

--- src/ch02/sec01/Misson2.java
+++ src/ch02/sec01/Misson2.java
@@ -2,7 +2,7 @@ package ch02.sec01;
 
 public class Misson2 {
     public static void main(String[] args) {
-        // 13 ~ 34 랜덤값 만들기
+        // 13 ~ 34 랜덤값 만들기...
         int rValue = (int)(Math.random() * 22.0) + 13;
 
         System.out.println(rValue);
